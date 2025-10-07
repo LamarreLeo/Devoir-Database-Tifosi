025f5714f97924da195ac9707f1f880afb68e290
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-10-08 01:34:45 +0200

    FIX - Suppréssion des '' sur le nom de la BDD pour corrigé une erreur

--- scripts/01_creation_schema.sql
+++ scripts/01_creation_schema.sql
@@ -17,9 +17,9 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- CRÉATION DE LA BASE DE DONNÉES
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS 'tifosi';
-CREATE SCHEMA IF NOT EXISTS `tifosi` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `tifosi` ;
+DROP SCHEMA IF EXISTS tifosi;
+CREATE SCHEMA IF NOT EXISTS tifosi DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE tifosi;
 
 -- -----------------------------------------------------
 -- CRÉATION DE L'UTILISATEUR TIFOSI
